3b94b39f0ccd3a3754b3c78b4ae4adf0d7bc20e3
William Isengard <william.isengard@gmail.com>
2019-01-16 13:52:27 -0700

    Updated Use Case with lists and descriptions

--- epic/use-case.php
+++ epic/use-case.php
@@ -7,9 +7,23 @@
 	<body>
 		<h1>Data Design Project</h1>
 			<h2>User Story</h2>
-				<p></p>
+				<p>As a user, Andy needs to network with key employees at local employers in her industry.</p>
 			<h2>Use Case</h2>
+				<ul>
+					<li><strong>Title:</strong> Andy Isengard</li>
+					<li><strong>Age:</strong> 26</li>
+					<li><strong>Gender:</strong> Female</li>
+					<li><strong>Occupation:</strong> Radiologic Technologist at Presbyterian Hospital Downtown</li>
+					<li><strong>Location:</strong> Albuquerque, NM</li>
+					<li><strong>Education:</strong> Associate's Degree</li>
+
+
 				<p>Andy has been at her current employer since graduating six months ago and is frustrated with her unstable and unpredictable schedule and part time hours. She needs to find a new workplace with a stable full-time schedule. Typical job posting sites are difficult to navigate and filled with low quality job postings.</p>
 			<h2>Interaction Flow</h2>
+				<ul>
+					<li>Difficulty finding current job openings at local employers.</li>
+					<li>Employer job sites are clunky and difficult to navigate.</li>
+					<li>Job search websites are filled with unreliable and out of date job postings.</li>
+				</ul>
 	</body>
 </html>
