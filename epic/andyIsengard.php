71a68d3e4ee3c647c695f0bb1cb778509367f5bf
William Isengard <william.isengard@gmail.com>
2019-01-16 13:19:29 -0700

    Created new nested list

--- epic/andyIsengard.php
+++ epic/andyIsengard.php
@@ -7,7 +7,7 @@
 	<body>
 		<h1>Data Design Project</h1>
 			<h2>User Persona</h2>
-				<img src="andyIsengard.php" alt="Andy Isengard" style="width:250px;height:250px;"/>
+				<img src="andyIsengard.jpg" alt="Andy Isengard" style="width:250px;height:250px;"/>
 				<ul>
 					<li><strong>Name:</strong> Andy Isengard</li>
 					<li><strong>Age:</strong> 26</li>
@@ -15,7 +15,14 @@
 					<li><strong>Occupation:</strong> Radiographer</li>
 					<li><strong>Location:</strong> Albuquerque, NM</li>
 					<li><strong>Education:</strong> Associate's Degree</li>
-					<li><strong>Technology:</strong> <em>Laptop:</em> 15" HP Envy. <em>OS:</em> Windows 10 Pro. <em>Browser:</em> Chrome. <em>Phone:</em> Samsung Galaxy S8. <em>Mobile Browser:</em> Chrome.</li>
+					<li><strong>Technology:</strong>
+						<ul>
+							<li><em>Laptop:</em> 15" HP Envy.</li>
+							<li><em>OS:</em> Windows 10 Pro.</li>
+							<li><em>Browser:</em> Chrome.</li>
+							<li><em>Phone:</em> Samsung Galaxy S8. </li>
+							<li><em>Mobile Browser:</em> Chrome.</li>
+						</ul>
 					<li><strong>Attitude:</strong> Infrequent user who does not network actively. Occasionally reads articles user's post, but does not post actvely.</li>
 				</ul>
 	</body>
