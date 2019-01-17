4781d139938719bba9e149447690d42a3f52d2c4
William Isengard <william.isengard@gmail.com>
2019-01-17 14:35:58 -0700

    Created SQL database

---
+++ sql/data-design.sql
@@ -0,0 +1,44 @@
+ALTER DATABASE data-design.sql CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+DROP TABLE IF EXISTS connection;
+DROP TABLE IF EXISTS employer;
+DROP TABLE IF EXISTS profile;
+
+CREATE TABLE profile (
+	profileId BINARY(16) NOT NULL,
+	profileActivationToken CHAR(32),
+	profileCurrentEmployer VARCHAR(32),
+	profileConnectionID char(32),
+	profileEducation VARCHAR(128),
+	profileEmail VARCHAR(128),
+	profileFirstName VARCHAR(128) NOT NULL,
+	profileHash CHAR(97) NOT NULL,
+	profileLastName VARCHAR(128) NOT NULL,
+	profileLocation VARCHAR(128) NOT NULL,
+	UNIQUE(profileId),
+	UNIQUE(profileEmail),
+	PRIMARY KEY(profileId)
+);
+
+CREATE TABLE employer (
+	employerId BINARY(16) NOT NULL,
+	employerFoundedDate DATETIME(6) NOT NULL,
+	employerIndustry VARCHAR(32) NOT NULL,
+	employerLocation VARCHAR(128) NOT NULL,
+	employerName VARCHAR(128) NOT NULL,
+	employerType VARCHAR(128) NOT NULL,
+	employerWebsite VARCHAR(128),
+	UNIQUE(employerId),
+	UNIQUE(employerName),
+	PRIMARY KEY(employerId)
+);
+
+CREATE TABLE connection (
+	tweetId BINARY(16) NOT NULL,
+	connectionProfileConnectionId BINARY(16) NOT NULL,
+	connectionProfileId BINARY(16) NOT NULL,
+	connectionDate DATETIME(6) NOT NULL,
+	FOREIGN KEY(connectionProfileConnectionID) REFERENCES profile(profileConnectionId),
+	FOREIGN KEY(connectionProfileID) REFERENCES profile(profileId),
+	PRIMARY KEY(tweetId)
+);
