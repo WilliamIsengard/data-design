2e2d984470c41fb7d4c8c202fcabc37330948ccd
William Isengard <william.isengard@gmail.com>
2019-01-17 12:37:53 -0700

    Added h4 headers and cleaned up code layout

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -8,49 +8,43 @@
 		<h1>Data Design Project</h1>
 			<h2>Conceptual Model</h2>
 				<h3>Entities & Attributes</h3>
-					<ul>
-						<li><strong>Profile</strong>
-							<ul>
-								<li>profileId (Primary Key)</li>
-								<li>profileActivationToken</li>
-								<li>profileCurrentEmployer</li>
-								<li>profileConnectionId</li>
-								<li>profileEducation</li>
-								<li>profileEmail</li>
-								<li>profileEmployeeId</li>
-								<li>profileFirstName</li>
-								<li>profileHash</li>
-								<li>profileLastName</li>
-								<li>profileLocation</li>
-							</ul>
-						</li>
-						<li><strong>Employer</strong>
-							<ul>
-								<li>employerId (Primary Key)</li>
-								<li>employerFoundedDate</li>
-								<li>employerIndustry</li>
-								<li>employerLocation</li>
-								<li>employerName</li>
-								<li>employerType</li>
-								<li>employerWebsite</li>
-							</ul>
-						</li>
-						<li><strong>Connection</strong>
-							<ul>
-								<li>connectionId (Primary Key)</li>
-								<li>connectionProfileConnectionId (Foreign Key)</li>
-								<li>connectionProfileID (Foreign Key)</li>
-								<li>connectionDate</li>
-							</ul>
-						</li>
-						<li><strong>Employee</strong>
-							<ul>
-								<li>employeeId (Primary Key)</li>
-								<li>employeeIdprofileEmployeeId (Foreign Key)</li>
-								<li>employeeIdDate</li>
-							</ul>
-						</li>
-					</ul>
+					<h4>Profile</h4>
+						<ul>
+							<li>profileId (Primary Key)</li>
+							<li>profileActivationToken</li>
+							<li>profileCurrentEmployer</li>
+							<li>profileConnectionId</li>
+							<li>profileEducation</li>
+							<li>profileEmail</li>
+							<li>profileEmployeeId</li>
+							<li>profileFirstName</li>
+							<li>profileHash</li>
+							<li>profileLastName</li>
+							<li>profileLocation</li>
+						</ul>
+					<h4>Employer</h4>
+						<ul>
+							<li>employerId (Primary Key)</li>
+							<li>employerFoundedDate</li>
+							<li>employerIndustry</li>
+							<li>employerLocation</li>
+							<li>employerName</li>
+							<li>employerType</li>
+							<li>employerWebsite</li>
+						</ul>
+					<h4>Connection</h4>
+						<ul>
+							<li>connectionId (Primary Key)</li>
+							<li>connectionProfileConnectionId (Foreign Key)</li>
+							<li>connectionProfileID (Foreign Key)</li>
+							<li>connectionDate</li>
+						</ul>
+					<h4>Employee</h4>
+						<ul>
+							<li>employeeId (Primary Key)</li>
+							<li>employeeIdProfileEmployeeId (Foreign Key)</li>
+							<li>employeeIdDate</li>
+						</ul>
 				<h3>Relationships</h3>
 					<ul>
 						<li>One <strong>Profile</strong> can have many <strong>Employers -(1 to n)</strong></li>
